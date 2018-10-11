2bbead22549c7057793218fecf58056eaf154ce6
Arnar Ólafsson <arnaro16@ru.is>
2018-10-11 20:52:52 +0000

    add er nuna tilbuid i a taka vid new line asamt kommu + tests

--- calculator.js
+++ calculator.js
@@ -3,13 +3,12 @@ function add(number){
     if(number == "")
     return 0;
 
-    if(number.includes(",") || number.includes("/n"))
+    if(number.includes(",") | number.includes(","))
     {
         var total = 0 ;
-        var numberArray = number.split(",");
+        var numberArray = number.split(/[,\n;]/);;
         for(var i = 0; i < numberArray.length; i++){
                 total += parseInt(numberArray[i]);
-            
         }   
         return total; 
     }
--- calculator.test.js
+++ calculator.test.js
@@ -12,7 +12,18 @@ it("should return number when only one number is in the string", () => {
 it("should return sum of two given numbers", () => {
     expect(add("1,2")).toBe(3);
 });
-it("should return sum of n given numbers", () => {
+it("should return sum of 3 given numbers", () => {
+    expect(add("1,2,3")).toBe(6);
+});
+it("should return sum of 4 given numbers", () => {
+    expect(add("1,2,3,4")).toBe(10);
+});
+it("should return sum of 5 given numbers", () => {
+    expect(add("1,2,3,4,5")).toBe(15);
+});
+it("should return sum of 6 given numbers", () => {
     expect(add("1,2,3,4,5,6")).toBe(21);
 });
-
+it("should return sum of n given numbers, even with \n", () => {
+    expect(add("1,2,3,4\n5,6")).toBe(21);
+});
